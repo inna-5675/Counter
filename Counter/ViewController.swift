966d126669ce94a84c1266ba030d5b3bcd6611db
Butkova Inna <inna-5675@mail.ru>
2024-12-17 18:19:57 +0500

    Вторая сдача задачи по третьему спринту

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -6,53 +6,59 @@
 //
 
 import UIKit
+import Foundation
+
+import UIKit
+import Foundation
 
 class ViewController: UIViewController {
     private var number: Int = 0
-    let date = Date()
-    let dateFrom = DateFormatter()
-    var historyArray = [String]()
-    
-        
-    @IBOutlet weak var plusOneButton: UIButton!
-    @IBOutlet weak var historyTextView: UITextView!
-    @IBOutlet weak var counterLabel: UILabel!
+    private var firstStringHistory = "История изменений:" {
+        didSet {
+            let oldText = historyTextView!
+            historyTextView.text = oldText.text + "\n" + firstStringHistory
+        }
+    }
+       
+    @IBOutlet weak private var plusOneButton: UIButton!
+    @IBOutlet weak private var historyTextView: UITextView!
+    @IBOutlet weak private var counterLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         counterLabel.text = String("Значение счетчика: \(number)")
-        historyTextView = historyArray()//вот тут не понимаю как добавить массив в TextView. Можно подсказку пожалуйста
-    }
+        
+       }
+   
     
-    
-    @IBAction func buttonDidTap(_ sender: Any) {
+    @IBAction private func buttonDidTap(_ sender: Any) {
         number += 1
         counterLabel.text = String("Значение счетчика: \(number)")
-        dateFrom.dateFormat = "dd MM yyyy, HH:mm"
-        historyArray.append("\(dateFrom)Значение изменено на +1/n")
+        firstStringHistory = "Значение изменено на +1"
     }
     
-    @IBAction func buttonMinusOne(_ sender: Any) {
+    @IBAction private func buttonMinusOne(_ sender: Any) {
         if number > 0 {
             number -= 1
-            historyArray.append("\(dateFrom) Значение изменено на -1/n")
+            firstStringHistory = "Значение изменено на -1"
         } else {
             number = 0
-            historyArray.append("\(dateFrom) попытка уменьшить значение счётчика ниже 0/n")
+            firstStringHistory = "Попытка уменьшить значение счётчика ниже 0"
         }
         counterLabel.text = String("Значение счетчика: \(number)")
         }
     
-    @IBAction func buttonPlusOne(_ sender: Any) {
+    @IBAction private func buttonPlusOne(_ sender: Any) {
         number += 1
         counterLabel.text = String("Значение счетчика: \(number)")
-        historyArray.append("\(dateFrom) Значение изменено на +1/n")
+        firstStringHistory = "Значение изменено на +1"
     }
-    @IBAction func buttonZeroTap(_ sender: Any) {
+    @IBAction private func buttonZeroTap(_ sender: Any) {
         number = 0
         counterLabel.text = String("Значение счетчика: \(number)")
-        historyArray.append("\(dateFrom) Значение сброшено/n")
+        firstStringHistory = "Значение сброшено"
     }
-    
    
 }
+
+
